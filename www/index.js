e5aef214db1a2264e81c96c06eeebf1930432e36
Damian Montero <photoca@hotmail.com>
2015-12-22 12:12:56 -0500

    new push

--- www/index.js
+++ www/index.js
@@ -1,16 +1,12 @@
 var http = require("http")
-var Pusher = require('pusher')
+var Pusher = require('pusher');
+var azure = require('azure');
+var azure = require('azure');
 var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
 
-app.use(express.static(__dirname + "/"))
-
-var server = http.createServer(app)
-server.listen(port)
-
-console.log("http server listening on %d", port)
-
+// Notification setups
 var pusher = new Pusher({
   appId: '162454',
   key: '559173cd02fac2d899db',
@@ -19,16 +15,31 @@ var pusher = new Pusher({
 });
 pusher.port = 443;
 
+var notificationHubService = azure.createNotificationHubService('hubname','connectionstring');
+
+
+// Form posts
 app.post("/www/api/PushTest",function(req,res) {
-  
    pusher.trigger('test_channel', 'my_event', {
   "message": req.body.message,
   "title": req.body.title
   }); 
-  
   res.send("Success. We sent everyone the notification.");
-    
 });
+
+
+
+
+// -- Regular web server stuff
+app.use(express.static(__dirname + "/"))
+
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+
+
 
 
 
