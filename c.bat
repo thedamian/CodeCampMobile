fa06f18c1d96bae2187af8d5c358b2ba47718967
Damian Montero <photoca@hotmail.com>
2016-01-11 13:07:12 -0500

    blackberry app

---
+++ c.bat
@@ -0,0 +1,13 @@
+@echo off
+Set BlackberryWebWorks="C:\Program Files (x86)\Research In Motion\BlackBerry WebWorks SDK 2.3.1.5"
+CLS
+del /q *.zip
+del /s /q app\*
+
+"C:\Program Files\7-Zip\7z.exe"  a %1.zip * -x!.git -x!images -x!google28917ba3591a728d.html -x!.gitignore -x!package.json -x!send.html -x!app.js -x!IISNode.yml -x!c.bat
+
+%BlackberryWebWorks%\bbwp %1.zip -v -o app -g 9545301745 
+REM -d for debug in web inspector
+pause
+%BlackberryWebWorks%\bin\JavaLoader.exe -w123456 load app\StandardInstall\%1.cod
+pause
