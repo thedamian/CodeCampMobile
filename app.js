3e7ff5eea8b64cedb7072c3f3412b6115d850bdc
Damian Montero <photoca@hotmail.com>
2017-03-09 12:28:06 -0500

    no more android

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ function checkForMobilePlatform(req,res) {
         res.redirect('/bb');
     }
 
-
+/*
     //Android
     if (/Android/.test(ua))
         //res.redirect('https://play.google.com/store/apps/details?id=com.threeguys1phone.codecampsfl');
