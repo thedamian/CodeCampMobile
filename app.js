12de2ee58fba925fc8d0ca2b5b367498750acea4
Damian Montero <photoca@hotmail.com>
2017-03-22 22:47:34 -0400

    android is back

--- app.js
+++ app.js
@@ -85,10 +85,7 @@ function checkForMobilePlatform(req,res) {
     //Android
     if (/Android/.test(ua))
         //res.redirect('https://play.google.com/store/apps/details?id=com.threeguys1phone.codecampsfl');
-        res.redirect('market://details?id=com.threeguys1phone.codecampsfl2');
-// */
-
-
+        res.redirect('market://details?id=com.threeguys1phone.codecampfl');
 
     // iOS
     if (/iPhone/.test(ua))
@@ -96,7 +93,6 @@ function checkForMobilePlatform(req,res) {
     
     if (/iPad/.test(ua))
         res.redirect('https://itunes.apple.com/us/app/south-florida-code-camp/id1070282332?ls=1&mt=8');    
-//    */ 
 
 }
 
