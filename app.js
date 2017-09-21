62f8fec8ab82f9f56cf118599b422016688efb35
Damian Montero <photoca@hotmail.com>
2017-09-21 12:32:48 -0400

    correction of android link

--- app.js
+++ app.js
@@ -83,8 +83,8 @@ function checkForMobilePlatform(req,res) {
 
     //Android
     if (/Android/.test(ua))
-        //res.redirect('https://play.google.com/store/apps/details?id=com.threeguys1phone.codecampsfl');
-        res.redirect('market://details?id=com.threeguys1phone.codecampfl');
+        res.redirect('http://play.google.com/store/apps/details?id=com.threeguys1phone.codecampfl');
+        //res.redirect('market://details?id=com.threeguys1phone.codecampfl');
 
     // iOS
     if (/iPhone/.test(ua))
